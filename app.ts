456ea91a926aaadbe40f557f1c14a2559c3bfacf
Cyrus-d <mdoaie@yahoo.co.uk>
2019-02-27 19:59:32 +0000

    test

---
+++ app.ts
@@ -0,0 +1,43 @@
+import * as Ajv from "ajv";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+import * as ts from "typescript";
+import { createFormatter } from "./factory/formatter";
+import { createParser } from "./factory/parser";
+import { createProgram } from "./factory/program";
+import { Config, DEFAULT_CONFIG } from "./src/Config";
+import { Schema } from "./src/Schema/Schema";
+import { SchemaGenerator } from "./src/SchemaGenerator";
+import { getRootTypes } from "./src/Utils";
+const basePath = "test/";
+const config: Config = {
+    type: "myFunction",
+    expose: "all",
+    topRef: true,
+    jsDoc: "extended",
+    path: resolve(`test.ts`),
+};
+const program: ts.Program = createProgram(config);
+const generator: SchemaGenerator = new SchemaGenerator(
+    program,
+    createParser(program, config),
+    createFormatter(config),
+);
+
+
+console.log(JSON.stringify(generator.createSchema("myFunction"), null, "  "));
+
+// const rootTypes = getRootTypes(program, config.path);
+// // @ts-ignore
+// let allSchema: Schema = { definitions: {} };
+// rootTypes.forEach((typ) => {
+//     const schema = generator.createSchema(typ);
+//     allSchema = {
+//         ...allSchema,
+//         definitions: {
+//             ...allSchema.definitions, ...schema.definitions,
+//         },
+//     };
+// });
+
+// console.log(JSON.stringify(allSchema, null, "  "));
