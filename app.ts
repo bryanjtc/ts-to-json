0ec5c7018a338fceca182a30f8b3033d0701ab25
Cyrus-d <mdoaie@yahoo.co.uk>
2020-02-29 19:49:53 +0330

    testing

--- app.ts
+++ app.ts
@@ -7,14 +7,31 @@ import { Config } from "./src/Config";
 
 import { SchemaGenerator } from "./src/SchemaGenerator";
 
+// const config: Config = {
+//     type: "*",
+//     expose: "none",
+//     topRef: true,
+//     jsDoc: "extended",
+//     path: resolve(`test.ts`),
+// };
+// const program: ts.Program = createProgram(config);
+// const generator: SchemaGenerator = new SchemaGenerator(program, createParser(program, config), createFormatter(config));
+
+// console.log(JSON.stringify(generator.createSchemaByNodeKind(ts.SyntaxKind.FunctionDeclaration, true), null, "  "));
 const config: Config = {
-    type: "*",
-    expose: "none",
-    topRef: true,
-    jsDoc: "extended",
     path: resolve(`test.ts`),
+    // type,
+    expose: "export",
+    topRef: true,
+    jsDoc: "none",
+    // extraTags: Config["extraTags"],
+    skipTypeCheck: true,
 };
+
 const program: ts.Program = createProgram(config);
+
 const generator: SchemaGenerator = new SchemaGenerator(program, createParser(program, config), createFormatter(config));
 
-console.log(JSON.stringify(generator.createSchemaByNodeKind(ts.SyntaxKind.FunctionDeclaration, true), null, "  "));
+const schema = generator.createSchema("MyObject1");
+
+console.log(JSON.stringify(schema, null, " "));
