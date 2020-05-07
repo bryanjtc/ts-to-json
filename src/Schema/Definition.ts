619bdc140b1a7dd6a26130616e7e7ce0e5fc4c10
Cyrus-d <mdoaie@yahoo.co.uk>
2020-05-07 17:26:47 +0430

    fixed types

--- src/Schema/Definition.ts
+++ src/Schema/Definition.ts
@@ -3,6 +3,9 @@ import { JSONSchema7, JSONSchema7Definition } from "json-schema";
 export interface Definition extends JSONSchema7 {
     propertyOrder?: string[];
     parameters?: { [key: string]: JSONSchema7 };
+    properties?: {
+        [key: string]: Definition | JSONSchema7Definition;
+    };
     defaultProperties?: string[];
     locale?: string;
     kind?: "function";
